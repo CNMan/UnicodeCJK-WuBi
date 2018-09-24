551118b5e56999657c2247781fb0a0ef9766c604
fromwheretowhere <fromwheretowhere.service@gmail.com>
2018-09-24 16:13:53 -0700

    添加搜索栏, 还需载入所有源数据

--- wubicodeeditor.py
+++ wubicodeeditor.py
@@ -4,15 +4,14 @@ import csv
 常量_源数据文件 = "CJK-A.txt"
 常量_修改后文件 = "修改" + 常量_源数据文件
 
-# TODO: 如果Unicode编码为2xxxx, 图片在Plane02中
 # 文件名格式统一为 U_xxxxxx.png （ xxxxxx 为 6 位 Unicode 编码，不足 6 位则前面补 0 ）
-常量_图片路径_花园明朝 = "FontGlyphs/HanaMin/Plane00/U_"
-常量_图片路径_汉仪仿宋 = "FontGlyphs/HYFangSong/Plane00/U_"
-常量_图片路径_汉仪字典宋 = "FontGlyphs/HYZiDianSong/Plane00/U_"
-常量_图片路径_细明体 = "FontGlyphs/MingLiU/Plane00/U_"
-常量_图片路径_细明体_HKSCS = "FontGlyphs/MingLiU_HKSCS/Plane00/U_"
-常量_图片路径_中易宋体 = "FontGlyphs/SimSun/Plane00/U_"
-常量_图片路径_中华书局宋体 = "FontGlyphs/ZhongHuaSong/Plane00/U_"
+常量_图片路径_花园明朝 = "FontGlyphs/HanaMin/"
+常量_图片路径_汉仪仿宋 = "FontGlyphs/HYFangSong/"
+常量_图片路径_汉仪字典宋 = "FontGlyphs/HYZiDianSong/"
+常量_图片路径_细明体 = "FontGlyphs/MingLiU/"
+常量_图片路径_细明体_HKSCS = "FontGlyphs/MingLiU_HKSCS/"
+常量_图片路径_中易宋体 = "FontGlyphs/SimSun/"
+常量_图片路径_中华书局宋体 = "FontGlyphs/ZhongHuaSong/"
 
 常量_图片扩展名 = ".png"
 
@@ -81,6 +80,15 @@ class Application(Frame):
         显示.pack(side = "right")
         return 可变值
 
+    # 如果Unicode编码大于4位, 图片在Plane02中
+    def 组成图片子路径(self, Unicode码):
+        Plane值 = "0"
+        if (len(Unicode码) > 4):
+            Plane值 = "2"
+        补0数 = 6 - len(Unicode码)
+        大写Unicode码 = "0" * 补0数 + Unicode码.upper()
+        return "Plane0" + Plane值 + "/U_" + 大写Unicode码 + 常量_图片扩展名
+
     def 创建控件(self):
         self.当前字符序号 = 0
         self.字符列表 = []
@@ -89,29 +97,26 @@ class Application(Frame):
         with open(常量_源数据文件, newline='') as 源数据文件:
             源数据读取器 = csv.reader(源数据文件, delimiter='\t')
             for 行 in 源数据读取器:
-                print(';'.join(行))
                 self.字符列表.append(行)
 
         self.当前字符 = self.字符列表[self.当前字符序号]
-
-        补0数 = 6 - len(self.当前字符[0])
-        大写Unicode码 = "0" * 补0数 + self.当前字符[0].upper()
+        self.图片子路径 = self.组成图片子路径(self.当前字符[0])
 
         图片区 = Frame(self)
         图片区.pack(side = "left")
         大陆字体区 = Frame(图片区)
         大陆字体区.pack()
-        self.图片显示1 = self.创建图片显示(大陆字体区, 常量_图片路径_中易宋体 + 大写Unicode码 + 常量_图片扩展名, "left")
-        self.图片显示2 = self.创建图片显示(大陆字体区, 常量_图片路径_中华书局宋体 + 大写Unicode码 + 常量_图片扩展名, "right")
+        self.图片显示1 = self.创建图片显示(大陆字体区, 常量_图片路径_中易宋体 + self.图片子路径, "left")
+        self.图片显示2 = self.创建图片显示(大陆字体区, 常量_图片路径_中华书局宋体 + self.图片子路径, "right")
 
         港台字体区 = Frame(图片区)
         港台字体区.pack()
-        self.图片显示3 = self.创建图片显示(港台字体区, 常量_图片路径_细明体_HKSCS + 大写Unicode码 + 常量_图片扩展名, "left")
-        self.图片显示4 = self.创建图片显示(港台字体区, 常量_图片路径_细明体 + 大写Unicode码 + 常量_图片扩展名, "right")
+        self.图片显示3 = self.创建图片显示(港台字体区, 常量_图片路径_细明体_HKSCS + self.图片子路径, "left")
+        self.图片显示4 = self.创建图片显示(港台字体区, 常量_图片路径_细明体 + self.图片子路径, "right")
 
         日本字体区 = Frame(图片区)
         日本字体区.pack()
-        self.图片显示5 = self.创建图片显示(日本字体区, 常量_图片路径_花园明朝 + 大写Unicode码 + 常量_图片扩展名, "top")
+        self.图片显示5 = self.创建图片显示(日本字体区, 常量_图片路径_花园明朝 + self.图片子路径, "top")
 
         细节区 = Frame(self)
         细节区.pack(side = "right")
@@ -139,10 +144,34 @@ class Application(Frame):
         下一个 = Button(遍历区, text = "下一个", command = self.下一个字符)
         下一个.pack(side = "right")
 
-        # TODO: 搜索/浏览功能
+        搜索区 = Frame(细节区)
+        搜索区.pack()
+        self.搜索Unicode值 = StringVar(value="")
+        Unicode值输入 = Entry(搜索区, textvariable=self.搜索Unicode值)
+        Unicode值输入.pack(side = "left")
+
+        搜索Unicode = Button(搜索区, text = "搜索Unicode", command = self.搜索Unicode)
+        搜索Unicode.pack(side = "right")
 
         导出按钮 = Button(细节区, text = "导出文件", command = self.导出文件)
         导出按钮.pack()
+
+    # 测试用: 3400, 2A000
+    # TODO: 避免线性查找
+    def 搜索Unicode(self):
+        Unicode值输入 = self.搜索Unicode值.get()
+        字符序号 = -1
+        已查到 = False
+        for 字符 in self.字符列表:
+            字符序号 += 1
+            if (Unicode值输入 == 字符[0]):
+                已查到 = True
+                break
+        if 已查到:
+          self.当前字符序号 = 字符序号
+          self.刷新控件()
+        else:
+          print("未找到Unicode码: " + Unicode值输入)
 
     def 刷新图片显示(self, 图片显示, 图片路径):
         图片 = PhotoImage(file=图片路径)
@@ -152,14 +181,13 @@ class Application(Frame):
     def 刷新控件(self):
         self.当前字符 = self.字符列表[self.当前字符序号]
         print("当前字符: " + str(self.当前字符))
-        补0数 = 6 - len(self.当前字符[0])
-        大写Unicode码 = "0" * 补0数 + self.当前字符[0].upper()
+        self.图片子路径 = self.组成图片子路径(self.当前字符[0])
         
-        self.刷新图片显示(self.图片显示1, 常量_图片路径_中易宋体 + 大写Unicode码 + 常量_图片扩展名)
-        self.刷新图片显示(self.图片显示2, 常量_图片路径_中华书局宋体 + 大写Unicode码 + 常量_图片扩展名)
-        self.刷新图片显示(self.图片显示3, 常量_图片路径_细明体_HKSCS + 大写Unicode码 + 常量_图片扩展名)
-        self.刷新图片显示(self.图片显示4, 常量_图片路径_细明体 + 大写Unicode码 + 常量_图片扩展名)
-        self.刷新图片显示(self.图片显示5, 常量_图片路径_花园明朝 + 大写Unicode码 + 常量_图片扩展名)
+        self.刷新图片显示(self.图片显示1, 常量_图片路径_中易宋体 + self.图片子路径)
+        self.刷新图片显示(self.图片显示2, 常量_图片路径_中华书局宋体 + self.图片子路径)
+        self.刷新图片显示(self.图片显示3, 常量_图片路径_细明体_HKSCS + self.图片子路径)
+        self.刷新图片显示(self.图片显示4, 常量_图片路径_细明体 + self.图片子路径)
+        self.刷新图片显示(self.图片显示5, 常量_图片路径_花园明朝 + self.图片子路径)
 
         self.Unicode编码值.set(self.当前字符[0])
         self.编码86版值.set(self.当前字符[2])
